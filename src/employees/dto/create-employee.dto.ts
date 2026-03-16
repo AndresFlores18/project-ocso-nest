869508075477b8fa56d2d1af7b908e6d5b9f1395
AndresFlores18 <aflores116@alumnos.uaq.mx>
2026-03-15 19:43:43 -0600

    Entity for employees

--- src/employees/dto/create-employee.dto.ts
+++ src/employees/dto/create-employee.dto.ts
@@ -1,6 +1,15 @@
+import { IsEmail, IsString, MaxLength } from "class-validator";
+
 export class CreateEmployeeDto {
-    id: string;
+    @IsString()
+    @MaxLength(30)
     name: string;
+    @IsString()
+    @MaxLength(70)
     lastname: string;
+    @MaxLength(10)
     phoneNumber: string;
+    @IsString()
+    @IsEmail()
+    email: string;
 }
